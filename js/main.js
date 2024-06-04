94f8c78e3a907c5971efd2f411dc003f363bfb13
ludoff <ludovicofiore25@gmail.com>
2024-06-04 15:24:53 +0200

    inizio progetto con diagramma di flusso

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+
+// richiesta numero di km da percorrere
+
+
+// richiesta età utente
+
+
+// calcolo prezzo in funzione dei km (0.21 €/km)
+
+
+// applicazione sconto in funzione dell'età
+
+    // se minorenne sconto 20%
+
+
+    // se over 65 sconto 40%
+
+
+    // altrimenti prezzo normale
+
+
+// aggiustamento prezzo finale con massimo due decimali 
+
+
+// output prezzo finale 
